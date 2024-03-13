95c98879a58e165131c972bbe8beccfadca429f5
gustavo.diprimio <gdprimio@sice.jujuy.edu.ar>
2024-03-12 21:05:17 -0300

    ips de render

--- server.js
+++ server.js
@@ -17,14 +17,21 @@ const uri = process.env.MONGO_URI
 
 const startServer = async () => {
     try {
-        connectDB(uri)
-        fastify.listen({port}, (err)=>{
-            if(err) {
-                console.error(err)
-            }
-            //console.log(`Server running in http://localhost:${port}/api/players`);
-            console.log(`¡Server running!`);
-        })        
+        //connectDB(uri)
+        // fastify.listen({port}, (err)=>{
+        //     if(err) {
+        //         console.error(err)
+        //     }
+        //     //console.log(`Server running in http://localhost:${port}/api/players`);
+        //     console.log(`¡Server running!`);
+        // })   
+        
+        connectDB(uri);
+        fastify.listen(port, {
+            host: '0.0.0.0',
+            ips: ['52.41.36.82', '54.191.253.12', '44.226.122.3']
+        });
+        console.log(`Server running on port ${port}`);
     } catch (err) {
         fastify.log.error(err)
     }
