ae04766f3d4a11f81803822adf7ae782b677d8af
infodpdev <infodpdev@gmail.com>
2024-03-12 22:32:52 -0300

    delete dotenv add listen new

--- server.js
+++ server.js
@@ -2,10 +2,6 @@ import Fastify from "fastify"
 import { connectDB } from "./database/db.js"
 import routes from "./routes/routes.js"
 
-import dotenv from 'dotenv';
-dotenv.config({path:'./.env'});
-
-
 const fastify = Fastify({
     logger:true
 })
@@ -17,17 +13,13 @@ const uri = process.env.MONGO_URI
 
 const startServer = async () => {
     try {
-        /* await connectDB(uri)
-        await fastify.listen({port}, (err)=>{
-            if(err) {
-                console.error(err)
-            }
-            //console.log(`Server running in http://localhost:${port}/api/players`);
-            console.log(`¡Server running!`);
-        }) */   
-        await connectDB(uri); // Espera a que la conexión a la base de datos se complete
-        await fastify.listen(port, '0.0.0.0'); // Ahora que la conexión a la base de datos está establecida, inicia el servidor
-        console.log(`Server running on port ${port}`);
+        await fastify.listen({ port, host: "0.0.0.0" }, (err, address) => {
+        if (err) {
+          fastify.log.error(err);
+        } else {
+          console.log(`Servidor running`);
+        }
+      });
     } catch (err) {
         fastify.log.error(err)
     }
